34bada72667166e3e6dfced5a96a30b4020ff001
Calen Mitchell <cmitch75@uncc.edu>
2018-03-02 02:32:26 +0000

    Finished section 4.7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -39,5 +39,18 @@ private
   # default: render 'new' template
 
 end 
+
+
+# in movies_controller.rb
+
+def create
+
+  @movie = Movie.create!(params[:movie])
+
+  flash[:notice] = "#{@movie.title} was successfully created."
+
+  redirect_to movies_path
+
+end
 end
   
