b920cccafe1655132f0d26410ed74aae37f49f9e
Calen Mitchell <cmitch75@uncc.edu>
2018-03-02 02:24:13 +0000

    finished section 4.6

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -31,5 +31,13 @@ private
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
+  
+  def new
+
+  @movie = Movie.new
+
+  # default: render 'new' template
+
+end 
 end
   
